e6118fa32f36675f066f12e309fe7aff82e214fc
Daniel Yepes <daniel.ortega@sharpgaming.com>
2024-01-11 14:33:47 -0500

    add var links to readme.md

--- 02-modern-javascript/test-app/src/index.js
+++ 02-modern-javascript/test-app/src/index.js
@@ -1,6 +1,6 @@
-import { testLetBehavior } from "./let.js";
-import { testConstBehavior } from "./const.js";
-import { testVarBehavior } from "./var.js";
+import { testLetBehavior } from "./const-and-variables/let.js";
+import { testConstBehavior } from "./const-and-variables/const.js";
+import { testVarBehavior } from "./const-and-variables/var.js";
 
 console.log('# 1. Constants and variables');
 
